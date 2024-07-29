23d5c72b6f237f90ba0f18d2f9cf2c4a39c6dbb9
jxlinnn <jenny.lin8302@gmail.com>
2024-07-29 17:18:39 -0400

    Update data_cleaning.py

--- Sentiment Classification/data_cleaning.py
+++ Sentiment Classification/data_cleaning.py
@@ -6,13 +6,20 @@ def filter_lang(data: List) -> List, List:
   english = []
   other = []
   for x,y in data:
-      try:
-          lang = detect(x)
-          if lang == 'en':
-              review_rating.append((x,y))
-      except:
-          others.append((x,y))
-    return english, other
+    try:
+        lang = detect(x)
+        if lang == 'en':
+            review_rating.append((x,y))
+    except:
+        others.append((x,y))
+  return english, other
+
+
+def wrap_text(s1: Series, s2: Series) -> List:
+  s1_after = s1.apply(lambda x: str(x).replace('\n', ' '))
+  data = [(x,y) for x,y in zip(s1_after, s2)]
+  return data
+
 
 def convert_to_csv(data: List, columns: List, csv_name: String):
   df = pd.DataFrame(data, columns=columns)
